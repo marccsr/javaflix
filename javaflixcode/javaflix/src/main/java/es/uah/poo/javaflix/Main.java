6475c539d31ccb205cc51497c0425c3ad8691167
marcos.garcia <marccsr.marcosgribalda@users.noreply.github.com>
2021-05-06 11:20:09 +0200

    Inicializo el proyecto.
    Creo el proyecto con el package es.uah.poo.javaflix y creo la primera clase Titulo (para herencia a peliculas y series) y la clase necesaria Actor para arraylist.

---
+++ javaflixcode/javaflix/src/main/java/es/uah/poo/javaflix/Main.java
@@ -0,0 +1,12 @@
+package es.uah.poo.javaflix;
+
+/**
+ *
+ * @author MARCOSTUF
+ */
+public class Main {
+    public static void main(String[]args){
+     
+    }
+    
+}
---
+++ javaflixcode/javaflix/src/main/java/es/uah/poo/javaflix/Titulo.java
@@ -0,0 +1,57 @@
+package es.uah.poo.javaflix;
+
+import java.util.ArrayList;
+
+public class Titulo {
+    private String titulo;
+    private String sinopsis;
+    private String genero;
+    private int año;
+    private ArrayList<Actor> actoresr;
+
+    public Titulo(ArrayList<Actor> actoresr) {
+        actoresr= new ArrayList<>();
+        this.año= año;
+        this.sinopsis=sinopsis;
+        this.genero=genero;
+        this.titulo=titulo;
+
+
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+    
+    public ArrayList<Actor> getActoresr() {
+        return actoresr;
+    }
+}
